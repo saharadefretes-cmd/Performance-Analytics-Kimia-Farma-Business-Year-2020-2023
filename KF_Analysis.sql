03636cb59186474102a7427eed09af13eef09c80
Sahara <saharadefretes@gmail.com>
2026-01-05 00:47:05 +0700

    Update KF_Analysis.sql

--- KF_Analysis.sql
+++ KF_Analysis.sql
@@ -1,9 +1,21 @@
 --Query Syntax Table Analysis OF Performance Analytics Kimia Farma Business Year 2020-2023
 CREATE TABLE Kimia_Farma.final_task_dataset AS
 SELECT 
-    x.transaction_id, x.date,x.branch_id, x.branch_name,x.kota,x.provinsi, x.rating_cabang, x.customer_name, x.product_id, x.product_name, x.actual_price,x.discount_percentage,
-    x.persentase_gross_laba, x.nett_sales,
-    -- Calculation for Nett Profit: (Actual Price * Gross Laba %) - (Actual Price - Nett Sales)
+    x.transaction_id,
+    x.date,x.branch_id, 
+    x.branch_name,
+    x.kota,
+    x.provinsi, 
+    x.rating_cabang,
+    x.customer_name, 
+    x.product_id, 
+    x.product_name,
+    x.actual_price,
+    x.discount_percentage,
+    x.persentase_gross_laba, 
+    x.nett_sales,
+    -- Calculation for Nett Profit: 
+  (Actual Price * Gross Laba %) - (Actual Price - Nett Sales)
     (x.actual_price * x.persentase_gross_laba) - (x.actual_price - x.nett_sales) AS nett_profit,
     x.rating as rating_transaksi,
 FROM (
